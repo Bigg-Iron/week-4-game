4038dcd8876d06cac34ac82f0caaf8683d1eacae
Bigg-Iron <lorenzor.bartolo@Lorenzos-MacBook-Pro.local>
2017-08-25 11:40:58 -0700

    Four different crystals populate in html on crystalize button click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,34 @@
 $(document).ready(function () {
 
     
-    // Appending the crystal image to the DOM
+    // Appending the crystal image 1 to the DOM
     $(".crystalizeBtn").click(function () {
-        var crystal = $('<img src="assets/images/crystal.jpg" class="img-responsive" alt="Ohh shiny...">')
-        $(".img-responsive").append(crystal);
+        var crystal = $('<img src="assets/images/crystal1.png" class="img-responsive" alt="Ohh shiny...">')
+        $(".crystal1").append(crystal);
+    });
+
+    // Appending the crystal image 1 to the DOM
+    $(".crystalizeBtn").click(function () {
+        var crystal = $('<img src="assets/images/crystal2.png" class="img-responsive" alt="Ohh shiny...">')
+        $(".crystal2").append(crystal);
+    });
+
+    // Appending the crystal image 1 to the DOM
+    $(".crystalizeBtn").click(function () {
+        var crystal = $('<img src="assets/images/crystal3.png" class="img-responsive" alt="Ohh shiny...">')
+        $(".crystal3").append(crystal);
+    });
+    
+    // Appending the crystal image 1 to the DOM
+    $(".crystalizeBtn").click(function () {
+        var crystal = $('<img src="assets/images/crystal4.png" class="img-responsive" alt="Ohh shiny...">')
+        $(".crystal4").append(crystal);
     });
     
     
-    
+    $(".btn").on('click', function() {
+        // Run this function on click
+    });
 
 
 
