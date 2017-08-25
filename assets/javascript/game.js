158458095da530bfbc09446abc7ae56feec6aa51
Bigg-Iron <lorenzor.bartolo@Lorenzos-MacBook-Pro.local>
2017-08-25 13:19:45 -0700

    When a crystal is clicked, an exclusive random integer is printed to console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,85 @@
 // jQuery document function 
 $(document).ready(function () {
 
-    
+    // Populates crystals in html on "crystalize" button 
+    // =======================================================================================
+
     // Appending the crystal image 1 to the DOM
     $(".crystalizeBtn").click(function () {
-        var crystal = $('<img src="assets/images/crystal1.png" class="img-responsive" alt="Ohh shiny...">')
+        var crystal = $('<img src="assets/images/crystal1.png" class="img-responsive crystal1" alt="Ohh shiny...">')
         $(".crystal1").append(crystal);
     });
 
-    // Appending the crystal image 1 to the DOM
+    // Appending the crystal image 2 to the DOM
     $(".crystalizeBtn").click(function () {
-        var crystal = $('<img src="assets/images/crystal2.png" class="img-responsive" alt="Ohh shiny...">')
+        var crystal = $('<img src="assets/images/crystal2.png" class="img-responsive crystal2" alt="Ohh shiny...">')
         $(".crystal2").append(crystal);
     });
 
-    // Appending the crystal image 1 to the DOM
+    // Appending the crystal image 3 to the DOM
     $(".crystalizeBtn").click(function () {
-        var crystal = $('<img src="assets/images/crystal3.png" class="img-responsive" alt="Ohh shiny...">')
+        var crystal = $('<img src="assets/images/crystal3.png" class="img-responsive crystal3" alt="Ohh shiny...">')
         $(".crystal3").append(crystal);
     });
-    
-    // Appending the crystal image 1 to the DOM
+
+    // Appending the crystal image 4 to the DOM
     $(".crystalizeBtn").click(function () {
-        var crystal = $('<img src="assets/images/crystal4.png" class="img-responsive" alt="Ohh shiny...">')
+        var crystal = $('<img src="assets/images/crystal4.png" class="img-responsive crystal4" alt="Ohh shiny...">')
         $(".crystal4").append(crystal);
     });
-    
-    
-    $(".btn").on('click', function() {
-        // Run this function on click
+
+
+
+
+
+
+    // Generates a random integer when user clicks a crystal, and logs it to the console.
+    // =======================================================================================
+
+    // Crystal 1 - Blue 
+    $(".crystal1").click(function () {
+
+        var randomInteger = Math.floor(Math.random() * (12 - 1)) + 1;
+
+        console.log("Blue crystal is worth " + randomInteger);
+
     });
+
+
+    // Crystal 2 - Green
+    $(".crystal2").click(function () {
+
+        var randomInteger = Math.floor(Math.random() * (12 - 1)) + 1;
+
+        console.log("Green crystal is worth " + randomInteger);
+
+    });
+
+
+    // Crystal 3 - Purple
+    $(".crystal3").click(function () {
+
+        var randomInteger = Math.floor(Math.random() * (12 - 1)) + 1;
+
+        console.log("Purple crystal is worth " + randomInteger);
+
+    });
+
+
+    // Crystal 4 - Pink
+    $(".crystal4").click(function () {
+
+        var randomInteger = Math.floor(Math.random() * (12 - 1)) + 1;
+
+        console.log("Pink crystal is worth " + randomInteger);
+
+    });
+
+
+
+
+
+
 
 
 
